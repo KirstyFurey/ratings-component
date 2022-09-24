13cf2de9023df3ecdda1b955a09564c3e38b2cba
Kirsty Furey <kirstyfurey.kf@gmail.com>
2022-09-24 16:40:05 +0100

    add function to move page on submit

---
+++ static/scripts/script.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+'use strict';
+const submit = document.getElementById('submit');
+const rate = document.getElementById('rate');
+const thanks = document.getElementById('ta');
+
+submit.onclick = () => {
+	rate.style.display = 'none';
+	thanks.style.display = 'block';
+}
